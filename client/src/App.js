d356483d2d52b91ef6093fa5fa31d7495c1de07d
Brewmaster <Juanherrera413@hotmail.com>
2021-07-12 15:53:07 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import navBar from './components/navBar';
+import Home from './components/pages/Home'
+{/* import router  npm install react-bootstrap bootstrap@4.6.0 */}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Router>
+      <navBar />
+        <Switch>
+          <Route path ='/' exact component = {Home}/>
+        </Switch>
+    </Router>
+    </>
   );
 }
 
