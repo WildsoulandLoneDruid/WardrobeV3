0832e2d441f5c8cfed2d0a179844db497c753419
Brewmaster <Juanherrera413@hotmail.com>
2021-07-14 09:02:28 -0400

    Delete wardrobe.js

--- server/models/wardrobe.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-export const wardrobeSchema = new mongoose.Schema ({
-    name: { type: String, unique: true}, { _id: true });
-
-  exports.MenuRoleModel = mongoose.model('MenuRoleModel', MenuRoleSchema, 'menu_role');
