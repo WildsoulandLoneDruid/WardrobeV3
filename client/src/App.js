777622c2d079f42858ec90586bbc01cd710b2182
Brewmaster <Juanherrera413@hotmail.com>
2021-07-15 09:05:57 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import navBar from './components/navBar';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import About from './components/pages/About'
+import SignedUp from './components/pages/SignedUp'
 {/* import router  npm install react-bootstrap bootstrap@4.6.0 */}
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <navBar />
         <Switch>
           <Route path ='/' exact component = {Home}/>
+          <Route path ='/services' exact component = {Services}/>
+          <Route path ='/about' exact component = {About}/>
+          <Route path ='/sign-up' exact component = {SignedUp}/>
         </Switch>
     </Router>
     </>
