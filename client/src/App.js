efff374e9713a4a2293d977b5a34169297970331
Brewmaster <Juanherrera413@hotmail.com>
2021-07-15 15:55:25 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,11 @@ import navBar from './components/navBar';
 import Home from './components/pages/Home'
 import Services from './components/pages/Services'
 import About from './components/pages/About'
-import SignedUp from './components/pages/SignedUp'
-{/* import router  npm install react-bootstrap bootstrap@4.6.0 */}
+import Signed_Up from './components/pages/Sign_up'
+{/* import router  npm install react-bootstrap bootstrap@4.6.0
+  npm install node-sass --save-dev
+  stylized-componetns
+*/}
 function App() {
   return (
     <> 
@@ -17,7 +20,7 @@ function App() {
           <Route path ='/' exact component = {Home}/>
           <Route path ='/services' exact component = {Services}/>
           <Route path ='/about' exact component = {About}/>
-          <Route path ='/sign-up' exact component = {SignedUp}/>
+          <Route path ='/sign-up' exact component = {Signed_Up}/>
         </Switch>
     </Router>
     </>
