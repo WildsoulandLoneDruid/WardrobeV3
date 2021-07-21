c0f2c823f5c0eceb0ef1555f363c19e0eb4f6327
Brewmaster <Juanherrera413@hotmail.com>
2021-07-21 09:23:22 -0400

    Add files via upload

--- server/api/updateDB.js
+++ server/api/updateDB.js
@@ -1,3 +1,4 @@
+ 
 const { Router } = require('express');
 const { listeners } = require('../models/user');
 
@@ -21,7 +22,7 @@ router.post('/addWardrobe', async(req, res, next) => {
                         "location":location
                     }
             }},{new: true,upsert:true}
-            ).sort({ 'location': "desc" }).exec(function(err, docs) {
+            ).sort({ 'totalNumberOfArticles': "desc" }).exec(function(err, docs) {
             if (err) {
                 next(err);
             } else {
@@ -50,7 +51,7 @@ router.post('/deleteWardrobe', async(req, res, next) => {
                 wardrobe:{
                     '_id': wardrobe_id
                 }
-        }}).sort({ 'location': "desc" }).exec(function(err, docs) {
+        }}).sort({ 'totalNumberOfArticles': "desc" }).exec(function(err, docs) {
         if (err) {
             next(err);
         } else {
@@ -142,7 +143,7 @@ router.post('/addArticle', async(req, res, next) => {
                     color: color,
                }
             }
-        },{new: true,upsert:true}).exec(function(err, docs) {
+        },{new: true,upsert:true}).sort({ 'timesUsed': "desc" }).exec(function(err, docs) {
             if (err) {
                 next(err);
             } else {
@@ -165,30 +166,49 @@ router.post('/removeArticle', async(req, res, next) => {
             wardrobe_id,
             RFID,
         } = req.body;
-        var update = util.updateNumberOfArticles(articleCheck, type, 0);
+        let article = await UserEntry.findOne({
+            "wardrobe._id":wardrobe_id
+        }).select({wardrobe:{$elemMatch:{_id:wardrobe_id}}})
+        
+        if (!article) {
+            throw new Error('Article not found');
+        }
+        // yup and all to be able to extract the data, I played with it for a while and this is what worked
+        article = article.toObject().wardrobe;
+        var update = util.updateNumberOfArticles(article[0].totalNumberOfShirts,article[0].totalNumberOfPants, type, 0);
         await UserEntry.findOneAndUpdate({
             '_id': _id,
-            'wardrobeData._id': wardrobe_id,
-        }, {
+            'wardrobe._id': wardrobe_id
+        },{
             $set: {
-                wardrobe:{
-                    'wardrobe.$.totalNumberOfShirts' : update.updateNumberOfShirts,
-                    'wardrobe.$.totalNumberOfPants' :  update.totalNumberofPants,
-                    'wardrobe.$.totalNumberOfArticles' : update.totalNumberofPants + update.totalNumberofShirts
-                }
-            },
-            $pull: {
-                wardrobe:{
-                   article:
-                   {
-                    'RFID': RFID
-                   }
-                }
-        }}).sort({ 'timesUsed': "desc" }).exec(function(err, docs) {
+                    'wardrobe.$.totalNumberOfShirts' : update[0],
+                    'wardrobe.$.totalNumberOfPants' :  update[1],
+                    'wardrobe.$.totalNumberOfArticles' : update[0] + update [1],
+            }
+        }).exec(function(err, docs) {
             if (err) {
                 next(err);
             } else {
-                console.log('Updated Article Wardobe: ' + docs);
+                console.log("Updated Amounts");
+            }
+        });
+        // up to here is working here
+        await UserEntry.findOneAndUpdate({
+            '_id': _id,
+            'wardrobe._id': wardrobe_id
+        }, {
+            $pull:
+            {
+               'wardrobe.$.articleData':
+               {
+                    RFID:RFID,
+               }
+            }
+        },{new: true,upsert:true}).sort({ 'timesUsed': "desc" }).exec(function(err, docs) {
+            if (err) {
+                next(err);
+            } else {
+                console.log('Removed Article: ' + docs);
                 res.status(200).json(docs);
             }
         })
@@ -199,41 +219,43 @@ router.post('/removeArticle', async(req, res, next) => {
         next(error);
     }
 });
-router.post('/updateArticle', async(req, res, next) => {
-    try {
-        const {
-            _id,
-            wardrobe_id,
-            RFID,
-            color,
-            type
-        } = req.body;
-        await UserEntry.findOneAndUpdate({
-            '_id': _id,
-            'wardrobeData._id': wardrobe_id,
-            'RFID': RFID
-        }, {
-            $set: {
-                    'wardrobe.$.article.$.color' : color,
-                    'wardrobe.$.article.$.type' :  type,
-                    'wardrobe.$.article.$.timesUsed' : 0,
-                    'wardrobe.$.article.$.active' : 'inactive'
-            }
-        }).exec(function(err, docs) {
-            if (err) {
-                next(err);
-            } else {
-                console.log('Updated Article Wardobe: ' + docs);
-                res.status(200).json(docs);
-            }
-        });
-    } catch (error) {
-        if (error.name === 'Validation Error') {
-            res.status(422);
-        }
-        next(error);
-    }
-});
+// router.post('/updateArticle', async(req, res, next) => {
+//     try {
+//         const {
+//             _id,
+//             wardrobe_id,
+//             RFID,
+//             color,
+//             type
+//         } = req.body;
+//         await UserEntry.findOneAndUpdate({
+//             '_id': _id,
+//             'wardrobeData._id': wardrobe_id,
+//             'RFID': RFID
+//         }, {
+//             $set: {
+//                     'wardrobe.$.articleData':
+//                     {
+//                         color : color,
+//                         type :  type,
+//                         timesUsed : 0,
+//                     }
+//             }
+//         }).exec(function(err, docs) {
+//             if (err) {
+//                 next(err);
+//             } else {
+//                 console.log('Updated Article Wardobe: ' + docs);
+//                 res.status(200).json(docs);
+//             }
+//         });
+//     } catch (error) {
+//         if (error.name === 'Validation Error') {
+//             res.status(422);
+//         }
+//         next(error);
+//     }
+// });
 router.post('/updateTimesUsed', async(req, res, next) => {
     try {
         const {
@@ -241,14 +263,38 @@ router.post('/updateTimesUsed', async(req, res, next) => {
             wardrobe_id,
             RFID,
         } = req.body;
-        const articleEntry = await UserEntry.findOneAndUpdate({
+        let currentTimesUsed = 0;
+        let article = await UserEntry.findOne({
+            "wardrobe._id":wardrobe_id
+        }).select({wardrobe:{$elemMatch:{_id:wardrobe_id}}})
+        
+        if (!article) {
+            throw new Error('Article not found');
+        }
+        // yup and all to be able to extract the data, I played with it for a while and this is what worked
+        article = article.toObject().wardrobe;
+        let currentStatus = article[0].articleData[0].status;      
+        if (currentStatus == 'A')
+        {
+            currentStatus == 'NA';
+            currentTimesUsed = article[0].articleData[0].timesUsed + 1;
+        } 
+        else
+        {
+            currentStatus == 'A';
+            currentTimesUsed = article[0].articleData[0].timesUsed;
+        }
+        await UserEntry.findOneAndUpdate({
                 '_id': _id,
                 'wardrobeData._id': wardrobe_id,
                 'RFID': RFID
             }, {
                 $set: {
-                    'wardrobe.$.articleData.$.timesUsed': articleEntry[0].wardrobe[0].articleData[0].timesUsed++,
-                    'wardrobeData.$.articleData.$.status': !(articleEntry[0].wardrobe[0].articleData[0].status),
+                    'wardrobe.$.articleData':
+                    {
+                        timesUsed: currentTimesUsed,
+                        status: currentStatus
+                    }
                 }
             }).sort({ 'timesUsed': "desc" })
             .exec(function(err, docs) {
@@ -266,4 +312,3 @@ router.post('/updateTimesUsed', async(req, res, next) => {
         next(error);
     }
 });
-module.exports = router;
