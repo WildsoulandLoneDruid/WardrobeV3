66144fc1ecb930cd5bc08b87d42a1f869dd48e3e
WildsoulandLoneDruid <wildsoulnation-mac@Juans-Air.lan>
2021-07-25 17:51:40 -0400

    ok

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -58,7 +58,7 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              WIS
+              PWA
               <i class='fab fa-typo3' />
             </Link>
           </div>
